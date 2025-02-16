8b335ef1aff88c81ed70a1294a9efaef51bbae74
Brayden Edwards <braydenedwards@Braydens-MacBook-Pro.local>
2025-02-16 00:06:28 -0800

    Classifier working

--- main.py
+++ main.py
@@ -12,29 +12,44 @@ def get_data(data_filename : str) -> np.ndarray:
     return X_train_df.to_numpy(), y_train_df.to_numpy(), X_test_df.to_numpy(), y_test_df.to_numpy()
 
 
-def in_interval(interval : list, indices : np.ndarray, first_inclusive : bool = True, second_inclusive : bool = False) -> bool:
-    start, end = interval
-    in_lower_bound = indices >= start if first_inclusive else indices > start
-    in_upper_bound = indices <= end if second_inclusive else indices < end
-    return in_lower_bound & in_upper_bound
-
-
-def classify(X_test : np.ndarray, P_hat : np.ndarray, m : int) -> np.ndarray:
-    return
+def is_in_interval(interval_i : list, interval_j : list, indices : np.ndarray, first_inclusive : bool = True, second_inclusive : bool = False) -> bool: 
     
+    start_i, end_i = interval_i
+    start_j, end_j = interval_j
 
+    if isinstance(indices, list) or indices.ndim == 1:
+        x_i_idx, x_j_idx = indices
+    else: 
+        x_i_idx = indices[:, 0]
+        x_j_idx = indices[:, 1]
 
-def main():
-    data_filename = "hw2_data.mat"
-    X_train, y_train, X_test, y_test = get_data(data_filename)
-    # m = [2,4,8,16]
-    # n = list(10**i for i in range(6)) # [10, 10^2, ..., 10^6]
+    in_lower_bound_i = x_i_idx >= start_i if first_inclusive else x_i_idx > start_i
+    in_upper_bound_i = x_i_idx <= end_i if second_inclusive else x_i_idx < end_i
     
-    # TESTING
-    m = 4
-    n = 10
+    in_lower_bound_j = x_j_idx >= start_j if first_inclusive else x_j_idx > start_j
+    in_upper_bound_j = x_j_idx <= end_j if second_inclusive else x_j_idx < end_j
+
+    is_point_in_interval = in_lower_bound_i & in_upper_bound_i & in_lower_bound_j & in_upper_bound_j
+
+    return is_point_in_interval
+
+
+def get_bin_indices(data_point : np.ndarray, m : int) -> tuple:
+    for i in range(1, m+1):
+        for j in range(1, m+1):
+            S_i = [(i - 1) / m, i / m]
+            S_j = [(j - 1) / m, j / m]
+            
+            if is_in_interval(S_i, S_j, data_point):
+                return i-1, j-1
+    
+    raise ValueError(f"Data point {data_point} does not belong to bin.")
+
+
+def get_probability_estimation(X_train : np.ndarray, y_train : np.ndarray, m : int) -> np.ndarray:
+    
     P_hat = np.empty((m,m))
-    # obtain box slices
+
     for i in range(1, m+1):
         for j in range(1, m+1):
             S_i = [(i - 1) / m, i / m]
@@ -43,10 +58,7 @@ def main():
             i_idx = i - 1
             j_idx = j - 1
             
-            x1_X_train = X_train[:, 0]
-            x2_X_train = X_train[:, 1]
-            
-            indices_points_in_bin = (in_interval(S_i, x1_X_train) & in_interval(S_j, x2_X_train))
+            indices_points_in_bin = is_in_interval(S_i, S_j, X_train)
             
             bin_points = X_train[indices_points_in_bin]
             bin_points_y_train = y_train[indices_points_in_bin]
@@ -58,17 +70,69 @@ def main():
                 P_hat[i_idx,j_idx] = num_bin_points_y_is_one / num_points_in_bin
             else: 
                 P_hat[i_idx,j_idx] = 0.5
-            
-            print(f"P_{i},{j} = {P_hat[i_idx,j_idx]}")
-            
-            
-            
-            
 
-            
+    return P_hat
 
 
-    
+def get_bayes_classifier(predictions : np.ndarray) -> np.ndarray:
+    classification = np.where(predictions > 0.5, 1, np.where(predictions < 0.5, -1, np.random.choice([-1, 1], size=predictions.shape))) # computing the classification
+    return classification
+
+
+def get_n_random_data_points(data : np.ndarray, n : int) -> np.ndarray:
+    random_indices = np.random.choice(data.shape[0], size=n, replace=False)
+    return data[random_indices]
+
+
+def get_expected_risk(classifications : np.ndarray, true_targets : np.ndarray) -> float:
+    total_values = classifications.shape[0]
+    correct_predictions = classifications == true_targets
+    total_correct_predictions = correct_predictions.sum()
+    return total_correct_predictions / total_values
+
+
+def main():
+    data_filename = "hw2_data.mat"
+    X_train, y_train, X_test, y_test = get_data(data_filename)
+    m_array = [2,4,8,16]
+    n_array = list(10**i for i in range(6)) # [10, 10^2, ..., 10^6]
+
+    for m in m_array:
+        for n in n_array:
+            num_monte_carlo_runs = 100
+            all_expected_risks = []
+
+            print(f"Expected Risk:")
+            print(f"(m = {m}, n = {n})") 
+
+            for i in range(num_monte_carlo_runs):
+                # obtaining sampled data
+                random_indices = np.random.choice(X_train.shape[0], size=n, replace=False)
+                sampled_X_train = X_train[random_indices]
+                sampled_y_train = y_train[random_indices]
+
+                # obtaining model
+                P_hat = get_probability_estimation(sampled_X_train, sampled_y_train, m) # getting probability of y = 1 per bin based on data
+
+                predictions = [] # initializing my predictions for the test data
+                for x1,x2 in X_test: # looping through each point in the sampled train data and getting thier bin index
+
+                    data_point = [x1, x2]
+                    i_idx, j_idx = get_bin_indices(data_point, m)
+
+                    predictions.append(P_hat[i_idx][j_idx]) # appending their p_hat values for that bin 
+
+                predictions = np.array(predictions).reshape(-1,1)
+                classifications = get_bayes_classifier(predictions)
+                expected_risk = get_expected_risk(classifications, y_test)
+                
+                print(f"{i}: {expected_risk}")
+                
+                all_expected_risks.append(expected_risk)
+                
+            print(f"Average Risk for m and n: {np.average(all_expected_risks)}")
+
+
 
 if __name__ == "__main__":
     main()
